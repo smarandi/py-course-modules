d57868077ca35ec2cc5c15191ee4e32c30ce8a53
smarandi <marandi.samuel@yahoo.com>
2018-03-19 15:41:42 +0530

    Module 4 complete

---
+++ Module 4/Problem4_1.py
@@ -0,0 +1,8 @@
+firstline = ["Happy", "families", "are", "all", "alike;", "every", \
+             "unhappy", "family", "is", "unhappy", "in", "its", "own", \
+             "way.", "Leo Tolstoy", "Anna Karenina"]
+#%%
+def problem4_1(wordlist):
+    print(wordlist)
+    wordlist.sort(key=str.lower)
+    print(wordlist)
